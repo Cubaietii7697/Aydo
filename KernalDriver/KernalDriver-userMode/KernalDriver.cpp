16bf6a9c4e1dd95f56fd92992d9632cd0090a7a4
dorian <foxg6607@gmail.com>
2025-09-25 05:11:12 +0300

    add main for user-mode

--- KernalDriver/KernalDriver-userMode/KernalDriver.cpp
+++ KernalDriver/KernalDriver-userMode/KernalDriver.cpp
@@ -1,19 +1,18 @@
-﻿// KernalDriver.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <print>
 
-#include <iostream>
+#include "Utils.hpp"
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "[usage] KernalDriver.exe <file-to-kill>" << std::endl;
+    return 1;
+  }
+  const std::filesystem::path &file = argv[1];
+  if (std::set<DWORD> pids = Utils::findProcess(file); !Utils::KillAllProcess(pids)) {
+    Utils::PrintError(L"didn't kill all because:");
+    return 1;
+  }
+  std::print("Successfully killed");
+  return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
